0c189ce7f3688c1aaf33efcc632750247d08f743
hao <lihaocaleo@gmail.com>
2019-11-22 05:54:58 -0500

    TODO: solve failed 2 tests

--- compare_version.py
+++ compare_version.py
@@ -1,4 +1,4 @@
-def compare_version(version_1,version_2):
+def compare(version_1,version_2):
 	try:
 		if(type(version_1) is not str or type(version_2) is not str):
 			return
@@ -8,39 +8,41 @@ def compare_version(version_1,version_2):
 	try:
 		version_1 = list(map(int, version_1.split('.')))
 		version_2 = list(map(int, version_2.split('.')))
-		print(version_1)
-		print(version_2)
 	except (ValueError):
 		print("Value Error: input contains non-digit items")
 		return
 
-	result = "0"
+	result = 0
 	# if version 1 is longer than version 2
 	if(len(version_1) > len(version_2)):
 		for i in range(len(version_2)):
 			if version_1[i] != version_2[i]:
 				if (version_1[i] - version_2[i] > 0):
-					result = "1"
+					result = 1
 					break
 				if (version_1[i] - version_2[i] < 0):
-					result = "-1"
+					result = -1
 					break
+		if(result != 0):
+			return result
 		for j in range(len(version_2), len(version_1)):
 			if(version_1[j]) > 0:
-				result = "1"
+				result = 1
 	# if version 2 is longer than version 1			
 	else:
 		for i in range(len(version_1)):
 			if version_1[i] != version_2[i]:
 				if (version_1[i] - version_2[i] > 0):
-					result = "1"
+					result = 1
 					break
 				if (version_1[i] - version_2[i] < 0):
-					result = "-1"
+					result = -1
 					break
+		if(result != 0):
+			return result
 		for j in range(len(version_1), len(version_2)):
 			if(version_2[j]) > 0:
-				result = "-1"
-	print(result)
+				result = -1
+	return result
 
 
---
+++ test_compare_version.py
@@ -0,0 +1,44 @@
+import unittest
+from compare_version import compare
+
+class Test_compare_version(unittest.TestCase):
+
+	def test_non_string_input(self):
+		with self.assertRaises(TypeError):
+			compare('1.2', 1.2)
+
+	def test_non_digit_input(self):
+		with self.assertRaises(ValueError):
+			compare('1.2.3', '1.2.#')
+
+	def test_same_len_less(self):
+		self.assertEqual(compare('1.2.3', '1.2.4'), -1)
+	def test_same_len_equal(self):
+		self.assertEqual(compare('1.2.3', '1.2.3'), 0)
+	def test_same_len_greater(self):
+		self.assertEqual(compare('1.2.4', '1.2.3'), 1)
+
+	def test_diff_len_less(self):
+		self.assertEqual(compare('1.2.3.4', '1.2.4'), -1)
+	def test_diff_len_equal(self):
+		self.assertEqual(compare('1.2.3.0', '1.2.3'), 0)
+	def test_diff_len_greater(self):
+		self.assertEqual(compare('1.2.4', '1.2.3.4'), 1)		
+
+	def test_same_len_less_wish_space(self):
+		self.assertEqual(compare('1.2.3.4 ', '1.2.4.4 '), -1)
+	def test_same_len_equal_with_space(self):
+		self.assertEqual(compare('1.2.3.4 ', '1.2.3.4 '), 0)
+	def test_same_len_greater_with_space(self):
+		self.assertEqual(compare('1.2.4.4 ', '1.2.3.4 '), 1)
+	
+	def test_diff_len_less_wish_space(self):
+		self.assertEqual(compare(' 1.2.3.4 ', '1.2.4.4 '), -1)
+	def test_diff_len_equal_with_space(self):
+		self.assertEqual(compare(' 1.2.3.4 ', '1.2.3.4 '), 0)
+	def test_diff_len_greater_with_space(self):
+		self.assertEqual(compare(' 1.2.4.4 ', '1.2.3.4 '), 1)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
